6d10a356b693bc66b1fc2bcad4e6456b6ba44cf4
yaswanth1997 <saiyaswanthchadalavada@gmail.com>
2023-10-05 11:27:26 -0700

    Rename Ec2 -1

--- Rename Ec2.py
+++ Rename Ec2.py
@@ -1,12 +1,11 @@
 import boto3
 
-def rename_instance(i-986345c789aef8, RenameInstance):
+def rename_instance(i-986345c789aef8, RenameInstance1):
 
     ec2 = boto3.resource('ec2')
     instance = ec2.Instance(i-986345c789aef8)
 
+    instance.create_tags(Tags=[{'Key': 'AWSbigdata', 'Value': RenameInstance1}])
 
-    instance.create_tags(Tags=[{'Key': 'AWSbigdata', 'Value': RenameInstance}])
-
-    print(f'Instance {i-986345c789aef8} renamed to {RenameInstance}.')
+    print(f'Instance {i-986345c789aef8} renamed to {RenameInstance1}.')
 
